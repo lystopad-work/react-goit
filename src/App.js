4eb0e5c0baa2247a6a520c10ac193ec51cbd8361
Mykyta Lystopad <listopad.work@gmail.com>
2023-07-05 18:15:53 +0300

    add new components from lesson 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {Card} from "./components/Css-example";
+import {StateComponent} from "./components/StateComponent";
 import './shared/styles/styles.scss';
 function App() {
 
   return (
       <div className="App">
-        <Card isCardHovered={true}/>
+        {/* <Card isCardHovered={true}/> */}
+        <StateComponent />
       </div>
   );
 }
